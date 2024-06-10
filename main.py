54138c5aa4a3c7617511948a3bd3a4e07121a503
taahahussain <114964689+taahahussain@users.noreply.github.com>
2024-06-10 09:15:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mlp
+import nltk
+
+import sklearn
+
+df = pd.read_csv('all_hadiths_clean.csv')
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
